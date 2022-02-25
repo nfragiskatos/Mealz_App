37ec0d46bd76cd9ac1e7febd351f45543702da64
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-02-25 16:15:23 -0700

    - restructured project package hierarchy - updated AndroidManifest.xml to point to new position of MainActivity.kt - created skeleton MealsRepository.kt - created skeleton MealsCategoriesViewModel.kt with reference to MealsRepository.kt - updated compose function names in MainActivity.kt - added lifecycle-viewmodel-compose dependency to project to me to bind a viewmodel to a composable's lifecycle when instantiating the viewmodel - added instantiated MealsCategoriesViewModel.kt in the MealsCategoriesScreen composable in MainActivity.kt

--- app/src/main/java/com/nfragiskatos/mealzapp/ui/meals/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/mealzapp/ui/meals/MainActivity.kt
@@ -1,15 +1,12 @@
-package com.nfragiskatos.mealzapp
+package com.nfragiskatos.mealzapp.ui.meals
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.nfragiskatos.mealzapp.ui.theme.MealzAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +14,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MealzAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MealsCategoriesScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MealsCategoriesScreen() {
+    val viewModel: MealsCategoriesViewModel = viewModel()
+    Text(text = "Hello!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MealzAppTheme {
-        Greeting("Android")
+        MealsCategoriesScreen()
     }
 }
---
+++ app/src/main/java/com/nfragiskatos/mealzapp/ui/meals/MealsCategoriesViewModel.kt
@@ -0,0 +1,7 @@
+package com.nfragiskatos.mealzapp.ui.meals
+
+import androidx.lifecycle.ViewModel
+import com.nfragiskatos.mealzapp.model.MealsRepository
+
+class MealsCategoriesViewModel(val repository: MealsRepository = MealsRepository()) : ViewModel() {
+}
