e2e45007cb65ed1c3bdbeabef5a91334db1bd8df
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-02-27 12:34:05 -0700

    - updated category card ui layout

--- app/src/main/java/com/nfragiskatos/mealzapp/ui/meals/MealCategoriesScreen.kt
+++ app/src/main/java/com/nfragiskatos/mealzapp/ui/meals/MealCategoriesScreen.kt
@@ -1,11 +1,20 @@
 package com.nfragiskatos.mealzapp.ui.meals
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberImagePainter
 import com.nfragiskatos.mealzapp.model.response.MealCategoryResponse
 import com.nfragiskatos.mealzapp.ui.theme.MealzAppTheme
 
@@ -14,16 +23,43 @@ fun MealsCategoriesScreen() {
     val viewModel: MealCategoriesViewModel = viewModel()
     val categories = viewModel.categoriesState.value
 
-    LazyColumn {
+    LazyColumn(contentPadding = PaddingValues(16.dp)) {
         items(categories) { category ->
-            Text(text = category.name)
+            MealCategory(category = category)
         }
     }
 }
 
 @Composable
 fun MealCategory(category: MealCategoryResponse) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = 2.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp)
+    ) {
+        Row() {
+            Image(
+                painter = rememberImagePainter(data = category.imageUrl),
+                contentDescription = "Category Image",
+                modifier = Modifier
+                    .size(88.dp)
+                    .padding(4.dp)
+            )
 
+            Column(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = category.name,
+                    style = MaterialTheme.typography.h6
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
